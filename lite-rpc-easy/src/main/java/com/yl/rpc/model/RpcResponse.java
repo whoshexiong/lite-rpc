ad6f047ec49f6a17886ecaaea69d47f9a91ae14b
yl <2636916275@qq.com>
2025-04-28 17:40:34 +0800

    实现调用服务的动态代理和rpc的请求处理器

---
+++ lite-rpc-easy/src/main/java/com/yl/rpc/model/RpcResponse.java
@@ -0,0 +1,6 @@
+package com.yl.rpc.model;/**
+ * @Author:   LyonYL 
+ * @Date:    2025/4/28
+ */ 
+public class RpcResponse {
+}
---
+++ lite-rpc-easy/src/main/java/com/yl/rpc/proxy/ServiceProxyFactory.java
@@ -0,0 +1,6 @@
+package com.yl.rpc.proxy;/**
+ * @Author:   LyonYL 
+ * @Date:    2025/4/28
+ */ 
+public class ServiceProxyFactory {
+}
